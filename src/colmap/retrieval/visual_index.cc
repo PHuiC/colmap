5244310a04fb3f90aadd03ed6839c6dd3d0a4b36
PHuiChen <1715005842@qq.com>
2025-10-28 20:00:59 +0800

    feat(retrieval): 开启词汇树索引多线程

--- src/colmap/retrieval/visual_index.cc
+++ src/colmap/retrieval/visual_index.cc
@@ -594,22 +594,29 @@ class FaissVisualIndex : public VisualIndex {
     faiss::IVFSearchParameters search_params;
     search_params.nprobe = num_checks;
 
-#pragma omp parallel num_threads(1)
-    {
-      omp_set_num_threads(GetEffectiveNumThreads(num_threads));
-#ifdef _MSC_VER
-      omp_set_nested(1);
-#else
-      omp_set_max_active_levels(1);
-#endif
+    index_->search(descriptors.rows(),
+                   descriptors.data(),
+                   num_neighbors,
+                   distances.data(),
+                   word_ids.data(),
+                   &search_params);
 
-      index_->search(descriptors.rows(),
-                     descriptors.data(),
-                     num_neighbors,
-                     distances.data(),
-                     word_ids.data(),
-                     &search_params);
-    }
+//#pragma omp parallel num_threads(1)
+//    {
+//      omp_set_num_threads(GetEffectiveNumThreads(num_threads));
+//#ifdef _MSC_VER
+//      omp_set_nested(1);
+//#else
+//      omp_set_max_active_levels(1);
+//#endif
+//
+//      index_->search(descriptors.rows(),
+//                     descriptors.data(),
+//                     num_neighbors,
+//                     distances.data(),
+//                     word_ids.data(),
+//                     &search_params);
+//    }
 
     return word_ids;
   }
